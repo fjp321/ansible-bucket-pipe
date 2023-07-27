2fc73b4f5f15dd690b91f122308225f62cb3505a
fjp321 <45053792+fjp321@users.noreply.github.com>
2023-07-26 20:34:37 -0400

    Update pipe.sh

--- pipe/pipe.sh
+++ pipe/pipe.sh
@@ -2,6 +2,25 @@
 source "$(dirname $0)/common.sh"
 info "Starting Pipe"
 
+INJECTED_SSH_CONFIG_DIR="/opt/atlassian/pipelines/agent/ssh"
+# The default ssh key with open perms readable by alt uids
+IDENTITY_FILE="${INJECTED_SSH_CONFIG_DIR}/id_rsa_tmp"
+# The default known_hosts file
+KNOWN_SERVERS_FILE="${INJECTED_SSH_CONFIG_DIR}/known_hosts"
+mkdir -p ~/.ssh || debug "adding ssh keys to existing ~/.ssh"
+touch ~/.ssh/authorized_keys
+info "Using default ssh key"
+cp ${IDENTITY_FILE} ~/.ssh/pipelines_id
+if [ ! -f ${KNOWN_SERVERS_FILE} ]; then
+  fail "No SSH known_hosts configured in Pipelines."
+fi
+cat ${KNOWN_SERVERS_FILE} >> ~/.ssh/known_hosts
+if [ -f ~/.ssh/config ]; then
+  debug "Appending to existing ~/.ssh/config file"
+fi
+echo "IdentityFile ~/.ssh/pipelines_id" >> ~/.ssh/config
+chmod -R go-rwx ~/.ssh/
+
 # default vars
 PLAYBOOK_NAME=${PLAYBOOK_NAME:="playbook.yml"}
 INVENTORY=${INVENTORY:="inventory"}
