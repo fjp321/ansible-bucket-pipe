f8561c8d64fc428c0db210cfeef42d7627b41df7
fjp321 <45053792+fjp321@users.noreply.github.com>
2023-07-26 22:24:59 -0400

    Update pipe.sh

--- pipe/pipe.sh
+++ pipe/pipe.sh
@@ -1,36 +1,48 @@
 #!/usr/bin/env bash
 source "$(dirname $0)/common.sh"
-info "Starting Pipe"
+info "Starting Pipe ..."
 
 # default vars
+info "Getting vars ..."
 PLAYBOOK_NAME=${PLAYBOOK_NAME:="playbook.yml"}
 INVENTORY=${INVENTORY:="inventory"}
-SSH_PRIVATE_KEY=${SSH_PRIVATE_KEY:=""}
 TAG=${TAG:=""}
 VAULT_PASSPHRASE=${VAULT_PASSPHRASE:=""}
+SSH_KEY_LOCATION=${SSH_KEY_LOCATION:=""}
 
 # get playbook
+info "Getting playbook ..."
 PLAYBOOK_FILE=$(find -P / -name ${PLAYBOOK_NAME} -not -path /usr/lib/* 2> /dev/null)
 PLAYBOOK_LOCATION=$(echo $PLAYBOOOK_FILE | sed 's/playbook.yml//')
-
-# goto playbook
+info "found playbook at $PLAYBOOK_LOCATION"
 cd $PLAYBOOK_LOCATION
 
 # create ansible command
+info "creating ansible command ..."
 ANSIBLE_COMMAND="ansible-playbook $PLAYBOOK_NAME -i inventory"
 
-if [$TAG != '']; then
+if [$TAG != ""]; then
+  info "Add tag ..."  
   ANSIBLE_COMMAND=$ANSIBLE_COMMAND+" -t $TAG"
+  info "done"
 fi
 
-if [$VAULT_PASSPHRASE != '']; then
+if [$VAULT_PASSPHRASE != ""]; then
+  info "add vault file ..."
   echo $VAULT_PASSPHRASE > vault_file
-  ANSIBLE_COMMAND=$ANSIBLE_COMMAND+" --vault-pass-file vault_file"
+  ANSIBLE_COMMAND="$ANSIBLE_COMMAND--vault-pass-file vault_file"
+  info "done"
 fi
 
-if [$SSH_PRIVATE_KEY != '']; then
-  ANSIBLE_COMMAND=$ANSIBLE_COMMAND+" --key-file $SSH_PRIVATE_KEY"
+if [$SSH_KEY_LOCATION != ""]; then
+  info "adding private key ..."
+  ANSIBLE_COMMAND="$ANSIBLE_COMMAND --key-file $SSH_KEY_LOCATION"
+  info "done"
 fi
+
+
+info "attempting ansible command ..."
+info "$ANSIBLE_COMMAND"
 
 bash -c "$ANSIBLE_COMMAND"
 
